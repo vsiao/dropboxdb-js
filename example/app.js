f9a37f2abccd077e30290e32f589598230232caf
Peter Xiao <peterthexiao@gmail.com>
2012-09-15 23:04:30 -0400

    fixed issue where __DBDB__ was still being returned in find

--- example/app.js
+++ example/app.js
@@ -62,7 +62,7 @@ app.get('/drop/:collection', function(req, res) {
 });
 
 app.get('/find/:collection', function(req, res) {
-  dropboxdb.find(req.params.collection, function(row) {return row['b'] === 'herp';}, function(res) {
+  dropboxdb.find(req.params.collection, function(row) {return true;}, function(res) {
     console.log(res);
   });
 });
